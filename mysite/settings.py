afe58786f752ee488177f7e2faf9824d89d43e7b
dresl <nikicresl@gmail.com>
2015-03-08 18:51:44 +0100

    set up database1 + change TIME_ZONE

--- mysite/settings.py
+++ mysite/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'dominik',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -68,7 +72,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Prague'
 
 USE_I18N = True
 
