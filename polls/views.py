601b7ecfe42f7bb6de1b27a9542c7b0f8ec24fcf
dresl <nikicresl@gmail.com>
2015-03-12 19:46:05 +0100

    solve css

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
-from django.core.wsgi import get_wsgi_application
-    from dj_static import Cling
 
-    application = Cling(get_wsgi_application())
 # Create your views here.
