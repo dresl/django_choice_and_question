0f1d97dc9b6af33c86313126bcfd94a96cd248bd
dresl <nikicresl@gmail.com>
2015-03-12 16:55:04 +0100

    models.py - change __str__ to __unicode__

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,12 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    def __str__(self):              # __unicode__ on Python 2
+    def __unicode__(self):              # __unicode__ on Python 2
         return self.question_text
 
 
@@ -11,5 +14,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __str__(self):              # __unicode__ on Python 2
+    def __unicode__(self):              # __unicode__ on Python 2
         return self.choice_text
