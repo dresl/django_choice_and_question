d76998f088046aee3fb9dadb53a6dce7541b5ad1
dresl <nikicresl@gmail.com>
2015-03-12 19:44:19 +0100

    solve css

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,6 +86,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = "/var/www/192.168.50.4/static/"
+STATIC_ROOT = 'static'
 
 ADMIN_MEDIA_PREFIX = '/static/admin/'
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.core.wsgi import get_wsgi_application
+    from dj_static import Cling
 
+    application = Cling(get_wsgi_application())
 # Create your views here.
