3583f37f70884cc518e5068c523134984b7aa628
dresl <nikicresl@gmail.com>
2015-03-12 19:11:59 +0100

    change static_rul + admin_media_prefix

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,4 +85,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/css/'
+STATIC_URL = '/static/'
+
+ADMIN_MEDIA_PREFIX = '/static/admin/' 
