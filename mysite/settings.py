c6bdcef8275168dfa8f9899014b88e6284d9aa08
dresl <nikicresl@gmail.com>
2015-03-14 18:12:34 +0100

    change static_root

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,7 +84,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = '/root/mysite/mysite/static/'
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
