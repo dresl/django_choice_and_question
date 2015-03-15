920b8fd7d1404558271f204b036848ba6ca1ab7c
dresl <nikicresl@gmail.com>
2015-03-15 18:32:45 +0100

    edit admin.py

--- polls/models.py
+++ polls/models.py
@@ -8,6 +8,11 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __unicode__(self):              # __unicode__ on Python 2 ; __str__ on Python 3
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
@@ -16,8 +21,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __unicode__(self):              # __unicode__ on Python 2 ; __str__ on Python 3
         return self.choice_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
