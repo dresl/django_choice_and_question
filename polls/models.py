eca9c72dc269fa3105a5aa21dc8c875cd7c6a15f
dresl <nikicresl@gmail.com>
2015-03-15 18:13:20 +0100

    edit published recently

--- polls/models.py
+++ polls/models.py
@@ -18,3 +18,6 @@ class Choice(models.Model):
         return self.choice_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
