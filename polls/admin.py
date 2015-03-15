98774db4b0d8cd7c4c77d116d6d1a129d10d21e6
dresl <nikicresl@gmail.com>
2015-03-15 16:25:25 +0100

    change admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from polls.models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
 
-# Register your models here.
+admin.site.register(Question, QuestionAdmin)
