2feff5425da1149b7f44ed80849f796e9218c2fa
dresl <nikicresl@gmail.com>
2015-03-20 17:09:02 +0100

    test

--- polls/urls.py
+++ polls/urls.py
@@ -11,5 +11,5 @@ urlpatterns = patterns('',
     url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>\d+)\.html$', views.html, name='html'),
 )
-
