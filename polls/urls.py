5c3e8a66a81cc2398b9e9cc2421ff6d22d6007e4
dresl <nikicresl@gmail.com>
2015-03-20 16:35:41 +0100

    change urls

--- polls/urls.py
+++ polls/urls.py
@@ -3,17 +3,5 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
-
-urlpatterns = patterns('',
-    # ex: /polls/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 )
