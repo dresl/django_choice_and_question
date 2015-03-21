c494f0e8e81c3e3700c07eab89fc486bab285b3d
dresl <nikicresl@gmail.com>
2015-03-21 22:47:00 +0100

    ...

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,5 +5,5 @@ from django.http import *
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^home', HttpResponse("You're voting on question")),
+    url(r'^home/', include('polls.urls')),
 )
