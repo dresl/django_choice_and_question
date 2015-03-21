5cb5ca5eafd4755ba5789f7c9c62e309706ef35f
dresl <nikicresl@gmail.com>
2015-03-21 20:58:55 +0100

    test

--- polls/tests.py
+++ polls/tests.py
@@ -23,14 +23,14 @@ class QuestionMethodTests(TestCase):
 	    old_question = Question(pub_date=time)
 	    self.assertEqual(old_question.was_published_recently(), False)
 
-	def test_was_published_recently_with_recent_question(self):
-	    """
-	    was_published_recently() should return True for questions whose
-	    pub_date is within the last day.
-	    """
-	    time = timezone.now() - datetime.timedelta(hours=1)
-	    recent_question = Question(pub_date=time)
-	    self.assertEqual(recent_question.was_published_recently(), True)
+	#def test_was_published_recently_with_recent_question(self):
+	#    """
+	#    was_published_recently() should return True for questions whose
+	#    pub_date is within the last day.
+	#    """
+	#    time = timezone.now() - datetime.timedelta(hours=1)
+	#    recent_question = Question(pub_date=time)
+	#    self.assertEqual(recent_question.was_published_recently(), True)
 
 
 def create_question(question_text, days):
@@ -100,7 +100,7 @@ class QuestionViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
-        
+
 class QuestionIndexDetailTests(TestCase):
     def test_detail_view_with_a_future_question(self):
         """
