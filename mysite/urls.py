6979aaf7dfae02dd4c4bf37db881b4d163887dcb
dresl <nikicresl@gmail.com>
2015-03-25 20:08:19 +0100

    ...

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import *
 from django.contrib import *
 from django.http import *
 
-import polls.views
-
 urlpatterns = patterns('',
 	url(r'^$', mysite.views.index, name='index'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
