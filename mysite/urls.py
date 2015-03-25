a057f19293d31eeb44afa308176c07cecf85264e
dresl <nikicresl@gmail.com>
2015-03-25 19:36:49 +0100

    ...

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@ from django.contrib import *
 from django.http import *
 
 urlpatterns = patterns('',
-	url(r'^$', polls.views.index, name='index'),
+	url(r'^$', mysite.views.index, name='index'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
