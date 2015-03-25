173952d713707bc6aded9fd2a74b509dc129cfbc
dresl <nikicresl@gmail.com>
2015-03-25 20:18:48 +0100

    .g.g

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
+	url(r'^$', views.index, name='index'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/info/$', views.InfoView.as_view(), name='info'),
     url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
